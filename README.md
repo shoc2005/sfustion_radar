5152a009a49254920a9fe48fd0910b585daa6acf
Andrejs <andrejs.zujevs@gmail.com>
2020-01-24 17:11:37 +0200

    Final project

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# sfustion_radar
-Matlab code for radar object detection.
+# Radar - final project
+
+## 2D CFAR algorithm description
+
+A constant false alarm rate (CFAR) estimation algorithm was implemented for the 2D case.
+ 
+The function 'subarea' creates a sliding window (a sub-area of a 2D matrix) for the current position in the matrix.
+The algorithm uses a loop (looping by rows and by columns of the matrix) which moves a sliding window within all elements of the matrix. Each position of the window represents an average value for training cells (that is implemented by applying a subtraction of the guard cells' sum value from the training cells' sum value and finally obtaining an average value under the sliding window). Next, the average value is used to determine a threshold value finally used in filtering of FFT values.
+
+## Selection of Training, Guard and offset
+Within some number of experiments were concluded that an offset of threshold value is best in range 1.3-1.5 due to maximal value after FFT is 45.0762. In the same time a number of Training and Guard cell used as small as possible to optimize performance of CFAR. Good results were obtained by Tr_num=2 an Guard_num = 2. This was possible due to samples of data contain relatively homogeneous level of the noise.
+
+The non-thresholded cells were managed by the implementation of the CFAR 2D algorithm. If the sliding window is outside the matrix missing elements were not used!
---
+++ radar-target-generation-and-detection.m
@@ -0,0 +1,248 @@
+clear all
+clc;
+%% Radar Specifications
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Frequency of operation = 77GHz
+% Max Range = 200m
+% Range Resolution = 1 m
+% Max Velocity = 100 m/s
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%fc = 77e9;
+R_max = 200;
+R_res = 1;
+V_max = 100;
+T_dist = 110; % distance to the target
+T_v = -20; % spped of the target
+%speed of light = 3e8
+c = 3e8;
+%% User Defined Range and Velocity of target
+% *%TODO* : define the target's initial position and velocity. Note : Velocity 
+% remains contant
+%% FMCW Waveform Generation
+
+% *%TODO* :
+%Design the FMCW waveform by giving the specs of each of its parameters.
+% Calculate the Bandwidth (B), Chirp Time (Tchirp) and Slope (slope) of the FMCW
+% chirp using the requirements above.
+B = c / (2 * R_res);
+Tchirp = 5.5 * (2 * R_max) / c;
+slope = B / Tchirp;
+
+%Operating carrier frequency of Radar 
+fc= 77e9;             %carrier freq
+
+                                                          
+%The number of chirps in one sequence. Its ideal to have 2^ value for the ease of running the FFT
+%for Doppler Estimation. 
+Nd=128;                   % #of doppler cells OR #of sent periods % number of chirps
+
+%The number of samples on each chirp. 
+Nr=1024;                  %for length of time OR # of range cells
+
+% Timestamp for running the displacement scenario for every sample on each
+% chirp
+t=linspace(0,Nd*Tchirp,Nr*Nd); %total time for samples
+
+
+%Creating the vectors for Tx, Rx and Mix based on the total samples input.
+Tx=zeros(1,length(t)); %transmitted signal
+Rx=zeros(1,length(t)); %received signal
+Mix = zeros(1,length(t)); %beat signal
+
+%Similar vectors for range_covered and time delay.
+r_t=zeros(1,length(t));
+td=zeros(1,length(t));
+
+%plot(td, t)
+%title('Signal Corrupted with Zero-Mean Random Noise')
+%% Signal generation and Moving Target simulation
+% Running the radar scenario over the time. 
+
+for i=1:length(t)         
+    
+    
+    % *%TODO* :
+    %For each time stamp update the Range of the Target for constant velocity. 
+    fb = (2 * T_dist * B) / (c * Tchirp);
+    td(i) = (fb * Tchirp) / B;
+    r_t(i) = (c * Tchirp * fb) / (2 * B);
+    
+    % *%TODO* :
+    %For each time sample we need update the transmitted and
+    %received signal. 
+    Tx(i) = cos(2 * pi * (fc * t(i) + (slope * t(i)^2) / 2));
+    Rx(i)  = cos(2 * pi * (fc * (t(i) - td(i)) + (slope * (t(i) - td(i))^2) / 2));
+    
+    % *%TODO* :
+    %Now by mixing the Transmit and Receive generate the beat signal
+    %This is done by element wise matrix multiplication of Transmit and
+    %Receiver Signal
+    Mix(i) = Tx(i) * Rx(i);
+    
+end
+%% RANGE MEASUREMENT
+
+ % *%TODO* :
+%reshape the vector into Nr*Nd array. Nr and Nd here would also define the size of
+%Range and Doppler FFT respectively.
+M_R = reshape(Mix, [Nr, Nd]);
+
+ % *%TODO* :
+%run the FFT on the beat signal along the range bins dimension (Nr) and
+%normalize.
+
+signal_fft = fft2(M_R, Nr, 1);
+signal_fft = signal_fft / max(signal_fft);
+
+ % *%TODO* :
+% Take the absolute value of FFT output
+signal_fft = abs(signal_fft)';
+
+ % *%TODO* :
+% Output of FFT is double sided signal, but we are interested in only one side of the spectrum.
+% Hence we throw out half of the samples.
+signal_fft  = signal_fft(1, 1:length(signal_fft)/2-1);
+
+
+%plotting the range
+figure ('Name', 'Range from First FFT')
+subplot(2,1,1)
+
+ % *%TODO* :
+ % plot FFT output 
+plot(signal_fft)
+ 
+axis ([0 200 0 1]);
+%% RANGE DOPPLER RESPONSE
+% The 2D FFT implementation is already provided here. This will run a 2DFFT 
+% on the mixed signal (beat signal) output and generate a range doppler map.You 
+% will implement CFAR on the generated RDM
+
+% Range Doppler Map Generation.
+
+% The output of the 2D FFT is an image that has reponse in the range and
+% doppler FFT bins. So, it is important to convert the axis from bin sizes
+% to range and doppler based on their Max values.
+
+Mix=reshape(Mix,[Nr,Nd]);
+
+% 2D FFT using the FFT size for both dimensions.
+sig_fft2 = fft2(Mix,Nr,Nd);
+
+% Taking just one side of signal from Range dimension.
+sig_fft2 = sig_fft2(1:Nr/2,1:Nd);
+sig_fft2 = fftshift (sig_fft2);
+RDM = abs(sig_fft2);
+RDM = 10*log10(RDM) ;
+
+%use the surf function to plot the output of 2DFFT and to show axis in both
+%dimensions
+doppler_axis = linspace(-100,100,Nd);
+range_axis = linspace(-200,200,Nr/2)*((Nr/2)/400);
+figure,surf(doppler_axis,range_axis,RDM);
+%% CFAR implementation
+
+%Slide Window through the complete Range Doppler Map
+
+% *%TODO* :
+%Select the number of Training Cells in both the dimensions.
+
+trc_num = 2;
+
+% *%TODO* :
+%Select the number of Guard Cells in both dimensions around the Cell under 
+%test (CUT) for accurate estimation
+
+guac_num = 2;
+
+% *%TODO* :
+% offset the threshold by SNR value in dB
+offset = 1.4;
+
+% *%TODO* :
+%Create a vector to store noise_level for each iteration on training cells
+noise_level = zeros(1,1);
+
+
+% *%TODO* :
+%design a loop such that it slides the CUT across range doppler map by
+%giving margins at the edges for Training and Guard Cells.
+%For every iteration sum the signal level within all the training
+%cells. To sum convert the value from logarithmic to linear using db2pow
+%function. Average the summed values for all of the training
+%cells used. After averaging convert it back to logarithimic using pow2db.
+%Further add the offset to it to determine the threshold. Next, compare the
+%signal under CUT with this threshold. If the CUT level > threshold assign
+%it a value of 1, else equate it to 0.
+[rows, cols] = size(RDM);
+RDM_final = zeros(rows, cols);
+
+
+for r=1:rows
+    for c=1:cols
+        % get guard cells sum value
+        [guard_value, g_c] = subarea(RDM, c, r, guac_num);
+        
+        % geet training cells sum value
+        [train_value, t_c] = subarea(RDM, c, r, guac_num + trc_num);
+        
+        train_value = (train_value - guard_value) / (t_c - g_c - 1);
+        train_value = pow2db(train_value);
+        
+        threshold_val = train_value * offset;
+        
+        if RDM(r,c) >= threshold_val
+            RDM_final(r,c) = 1;
+        end
+    end
+end
+
+
+   % Use RDM[x,y] as the matrix from the output of 2D FFT for implementing
+   % CFAR
+
+
+
+
+
+% *%TODO* :
+% The process above will generate a thresholded block, which is smaller 
+%than the Range Doppler Map as the CUT cannot be located at the edges of
+%matrix. Hence,few cells will not be thresholded. To keep the map size same
+% set those values to 0. 
+ 
+% This point is not necessary due to the sliding window care about this!
+
+
+
+
+% *%TODO* :
+%display the CFAR output using the Surf function like we did for Range
+%Doppler Response output.
+figure,surf(doppler_axis,range_axis,RDM_final);
+colorbar;
+surface(trc_num);
+axis ([-100 100 -200 200 0 1]);
+%%
+function [value, elem_c] = subarea(data, pos_x, pos_y, distance)
+    % the function estimates and avg value for the specified submatrix
+    
+    % get min/max indices
+    min_y = max([pos_y - distance, 1]);
+    max_y = min([size(data,1), pos_y + distance]);
+    
+    min_x = max([pos_x - distance, 1]);
+    max_x = min([size(data,2), pos_x + distance]);
+    
+    submatrix = data(min_y:max_y, min_x:max_x);
+        
+    [r , c] = size(submatrix);
+    
+    newsubmatrix = db2pow(submatrix); % convert to linear space
+    submatrix(pos_y, pos_x) = 0.0; % remove the CUT value
+    
+    value = sum(newsubmatrix, 'all'); % get total sum
+    elem_c = r * c;
+    
+end
